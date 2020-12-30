d1114f509459a0440558bd8443e90507abff1de1
sophia <73413312+sophia0110@users.noreply.github.com>
2020-12-30 18:05:39 +0800

    member_card

---
+++ src/components/MemberCard.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import '../style/default.scss'
+import { Card } from 'react-bootstrap'
+import '../style/member_card.scss'
+import { MdStar, MdStarBorder, MdStarHalf, MdCameraAlt } from 'react-icons/md'
+// var Rating = require('react-rating')
+// React.createElement(Rating)
+function MemberCard() {
+  return (
+    <>
+      <div className="container ">
+        <Card className="card">
+          <div className="d-flex justify-content-center align-items-center">
+            <Card.Img
+              className=" rounded-circle "
+              style={{ width: '160px' }}
+              variant="top"
+              src="./pic/pic/member.jpg"
+              alt=""
+            />
+            <div class="position-relative">
+              <MdCameraAlt
+                className=" position-absolute top-100 start-100 translate-middle "
+                style={{
+                  width: '42px',
+                  height: '42px',
+                  top: '40px',
+                  right: '-5px',
+                  color: '#104b6d',
+                }}
+              />
+            </div>
+          </div>
+
+          <Card.Body>
+            <Card.Text className="d-flex justify-content-center ">
+              <p>2.5 </p>
+              <div className="star ">
+                <MdStar />
+                <MdStar />
+                <MdStarHalf />
+                <MdStarBorder />
+                <MdStarBorder />
+              </div>
+            </Card.Text>
+          </Card.Body>
+          {/* <Card.Footer>
+            <small className="text-muted">5.0 我是評分系統</small>
+          </Card.Footer> */}
+        </Card>
+      </div>
+    </>
+  )
+}
+
+export default MemberCard
