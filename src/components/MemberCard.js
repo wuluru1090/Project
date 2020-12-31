b11e238e591d54c683137b581d7b5062f402df3c
sophia <73413312+sophia0110@users.noreply.github.com>
2020-12-31 11:48:24 +0800

    add MemberNavlist

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../style/default.scss'
-import { Card, Nav } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 import '../style/member_card.scss'
 import { MdStar, MdStarBorder, MdStarHalf, MdCameraAlt } from 'react-icons/md'
 // var Rating = require('react-rating')
@@ -49,44 +49,6 @@ function MemberCard() {
             <small className="text-muted">5.0 我是評分系統</small>
           </Card.Footer> */}
         </Card>
-
-        <br />
-
-        <Nav defaultActiveKey="/home" className="flex-column  mem_navlist">
-          <Nav.Link
-            href="/home"
-            className=" d-flex align-items-center justify-content-around"
-          >
-            <div className="mem_listbar">
-              <MdStarBorder />
-            </div>
-            <h6>會員資料</h6>
-          </Nav.Link>
-          <Nav.Link eventKey="link-1">
-            <h6>我的揪團</h6>
-          </Nav.Link>
-          <Nav.Link eventKey="link-2">
-            <h6>訂單管理</h6>
-          </Nav.Link>
-          <Nav.Link eventKey="link-3">
-            <h6>折價券</h6>
-          </Nav.Link>
-          <Nav.Link eventKey="link-4">
-            <h6>我的收藏</h6>
-          </Nav.Link>
-          <Nav.Link eventKey="link-5">
-            <h6>我的相簿</h6>
-          </Nav.Link>
-          <Nav.Link eventKey="link-6">
-            <h6>歷史紀錄</h6>
-          </Nav.Link>
-          <Nav.Link eventKey="link-7">
-            <h6>我的評價</h6>
-          </Nav.Link>
-          <Nav.Link eventKey="link-8">
-            <h6>帳號管理</h6>
-          </Nav.Link>
-        </Nav>
       </div>
     </>
   )
