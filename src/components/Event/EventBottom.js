a098e39b44350c3bffa01dca884748e26b612216
wuluru0901 <73413247+wuluru1090@users.noreply.github.com>
2021-01-04 23:49:53 +0800

    活動頁卡片切換顯示

--- src/components/Event/EventBottom.js
+++ src/components/Event/EventBottom.js
@@ -1,52 +1,58 @@
 import React from 'react'
 import '../../style/default.scss'
-import '../../style/event/event_search.scss'
+import '../../style/event/event_bottom.scss'
 import { devUrl } from '../../config'
 
 function EventBottom() {
   return (
     <>
-      <div className="blue-wave-with-couple">
-        <img src={devUrl + '/Pic/SVG/wave-blue-1440.svg'}></img>
-      </div>
-      <div className="wave-height">
-        <div className="theme d-flex justify-content-center">
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            風景攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            人像攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            商業攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            生態攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            建築攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            旅遊攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            美食攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            夜景攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            人文攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            手機攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            水中攝影
-          </a>
-          <a href="#" className="btn rounded-pill theme-button" type="button">
-            其他類型
-          </a>
+      <div className="event-wrapper">
+        <div className="blue-wave-with-couple">
+          {/* <img className="couple" src={devUrl + '/Pic/SVG/couple.svg'}></img> */}
+          <img
+            className="blue-wave"
+            src={devUrl + '/Pic/SVG/wave-blue-with-couple-1440-01.svg'}
+          ></img>
+        </div>
+        <div className="wave-height">
+          <div className="theme d-flex justify-content-center">
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              風景攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              人像攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              商業攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              生態攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              建築攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              旅遊攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              美食攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              夜景攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              人文攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              手機攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              水中攝影
+            </a>
+            <a href="#" className="btn rounded-pill theme-button" type="button">
+              其他類型
+            </a>
+          </div>
         </div>
       </div>
     </>
--- src/components/Event/EventResult.js
+++ src/components/Event/EventResult.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Pagination from './Pagination'
 import '../../style/default.scss'
 import '../../style/event/event_result.scss'
 import { devUrl } from '../../config'
 import EventCardVer from './EventCardVer'
+import EventCardHor from './EventCardHor'
 
 function EventResult() {
+  const [displayCard, setDisplayCard] = useState(true)
   return (
     <>
       <div className="event-container">
@@ -13,16 +15,38 @@ function EventResult() {
           <div className="result-word">
             <h6 className="d-inline-block">搜尋結果 共16筆</h6>
           </div>
-          <div className="icon d-inline-block">
+          <div className="result-icon d-inline-block">
             <img
-              className="d-inline-block c-icon"
+              className=" c-icon"
               src={devUrl + '/Pic/SVG/d-card-unselected.svg'}
               alt=""
+              onClick={() => setDisplayCard(true)}
+              style={
+                displayCard
+                  ? { display: 'none' }
+                  : { display: 'inline-block', marginRight: '44px' }
+              }
             />
             <img
-              className="d-inline-block c-icon"
+              className=" c-icon card-selected"
+              src={devUrl + '/Pic/SVG/d-card-selected.svg'}
+              alt=""
+              onClick={() => setDisplayCard(false)}
+              style={{ display: displayCard ? 'inline-block' : 'none' }}
+            />
+            <img
+              className=" c-icon"
               src={devUrl + '/Pic/SVG/d-list-unselected.svg'}
               alt=""
+              onClick={() => setDisplayCard(false)}
+              style={{ display: displayCard ? 'inline-block' : 'none' }}
+            />
+            <img
+              className=" c-icon list-selected"
+              src={devUrl + '/Pic/SVG/d-list-selected.svg'}
+              alt=""
+              onClick={() => setDisplayCard(true)}
+              style={{ display: displayCard ? 'none' : 'inline-block' }}
             />
           </div>
         </div>
@@ -30,42 +54,45 @@ function EventResult() {
         {/* <div className="no-result">
           <h5>很抱歉，未找到符合的搜尋結果。</h5>
         </div> */}
-        <div className="have-result d-flex row justify-content-center">
-          <div className="col-3 d-flex justify-content-center">
+        <div
+          className="have-result d-flex row justify-content-center"
+          style={
+            displayCard
+              ? { display: 'block' }
+              : { height: 0, width: 0, opacity: 0 }
+          }
+        >
+          <div className="result-card d-flex row justify-content-start">
+            <EventCardVer />
+            <EventCardVer />
+            <EventCardVer />
+            <EventCardVer />
+            <EventCardVer />
+            <EventCardVer />
+            <EventCardVer />
+            <EventCardVer />
+            <EventCardVer />
+            <EventCardVer />
             <EventCardVer />
           </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
-          </div>
-          <div className="col-3 d-flex justify-content-center">
-            <EventCardVer />
+        </div>
+        <div
+          className="have-result d-flex row justify-content-center"
+          style={
+            displayCard
+              ? { height: 0, width: 0, opacity: 0 }
+              : { display: 'block' }
+          }
+        >
+          <div className="result-list d-flex justify-content-start flex-wrap">
+            <EventCardHor />
+            <EventCardHor />
+            <EventCardHor />
+            <EventCardHor />
+            <EventCardHor />
+            <EventCardHor />
+            <EventCardHor />
+            <EventCardHor />
           </div>
         </div>
         <Pagination />
